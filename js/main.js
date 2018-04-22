19571e644f2908db97dd9c6316db6d14a8ffbc6e
Fernando Gomez <gomezhyuuga@gmail.com>
2018-04-22 01:08:09 -0500

    English version, plus changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var SEQUENTIAL_TIME = 0.00;
+var PARALLEL_TIME = 0.00;
+
 $(function () {
   console.log('it works');
 
@@ -5,22 +8,24 @@ $(function () {
 });
 
 function reset() {
-  $('#result').html("Calculando resultado...");
+  $('#result').html("Computing result...");
   $('#performance').html("0.00");
 }
 function compute(event) {
   reset();
 
+  var parallel   = event.target.dataset.parallel == 1;
+  var method     = parallel ? "parallel" : "sequential";
   var start_time = performance.now();
-  var limit = +$('#limit').val() || 1e8; limit++;
-  var thread_no = (this.id === "computeSeq") ? 1 : +$('#thread_no').val();
+  var limit      = +$('#limit').val() || 1e8; limit++;
+  var thread_no  = (this.id === "computeSeq") ? 1 : +$('#thread_no').val();
 
   var finished = 0;
   for (var i = 0; i < thread_no; i++) {
     var worker = new Worker('js/coprime_worker.js')
     var size   = Math.floor(limit / thread_no);
-    var start = i * size;
-    var end = (i === thread_no - 1) ? limit : (i + 1) * size;
+    var start  = i * size;
+    var end    = (i === thread_no - 1) ? limit : (i + 1) * size;
 
     worker.postMessage({
       start: start,
@@ -34,9 +39,20 @@ function compute(event) {
 
       if (finished === thread_no) {
         var end_time = performance.now();
-        var time = (end_time - start_time) / 1000;
-        $('#result').html(result);
-        $('#performance').html(time);
+        var time     = (end_time - start_time) / 1000;
+
+        if (parallel) PARALLEL_TIME = time;
+        else SEQUENTIAL_TIME = time;
+
+        time   = accounting.formatNumber(time, 5);
+        result = accounting.formatNumber(result);
+
+        $("#result").html(result);
+        $('#time-' + method).html("<pre>" + time + " secs</pre>");
+
+        var speedup = (SEQUENTIAL_TIME && PARALLEL_TIME) ? SEQUENTIAL_TIME / PARALLEL_TIME : 0.00;
+        speedup = accounting.formatNumber(speedup, 2);
+        $("#speedup").html(speedup + "x");
       }
     }
   }
